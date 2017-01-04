106b534b0427f5919377ed9a076dc36767ad7227
Petr Sima <pt@Petrs-MacBook-Pro.local>
2017-01-04 12:56:33 +0100

    migrated webkit extensions

--- ACKReactiveExtensions/WebKit/WebKitExtensions.swift
+++ ACKReactiveExtensions/WebKit/WebKitExtensions.swift
@@ -3,6 +3,12 @@ import ReactiveSwift
 import ReactiveCocoa
 import ACKReactiveExtensions
 
+extension Reactive where Base: WKWebView {
+    var estimatedProgress: Property<CGFloat> {
+        return Property(base.rac_estimatedProgress.map { $0 ?? 0 })
+    }
+}
+
 private enum WebKitKeys {
     static var estimatedProgress = UInt8(0)
 }
@@ -12,6 +18,7 @@ extension WKWebView {
     /**
      * Property which observes estimated progress of request load.
      */
+    @available(*, deprecated, renamed: "reactive.estimatedProgress")
     public var rac_estimatedProgress: DynamicProperty<CGFloat> {
         return lazyAssociatedProperty(self, &WebKitKeys.estimatedProgress, factory: { [unowned self] _ in
             return DynamicProperty<CGFloat>(object: self, keyPath: "estimatedProgress")
