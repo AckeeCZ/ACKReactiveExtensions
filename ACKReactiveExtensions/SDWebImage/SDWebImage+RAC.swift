68dcc93e67c7253fec5d2c7cda46c7985ef6fcd3
Petr Sima <pt@Petrs-MacBook-Pro.local>
2017-01-04 12:48:14 +0100

    migrated SDWebImage extensions

--- ACKReactiveExtensions/SDWebImage/SDWebImage+RAC.swift
+++ ACKReactiveExtensions/SDWebImage/SDWebImage+RAC.swift
@@ -2,11 +2,38 @@ import UIKit
 import ReactiveSwift
 import SDWebImage
 
+
+extension Reactive where Base: UIImageView {
+    public var imageURL: BindingTarget<URL?> {
+        return makeBindingTarget { $0.sd_setImage(with: $1, placeholderImage: nil) }
+    }
+}
+
+extension Reactive where Base: SDWebImageDownloader {
+    public func downloadImage(with url: URL, options: SDWebImageDownloaderOptions = SDWebImageDownloaderOptions()) -> SignalProducer<UIImage, NSError> {
+        return SignalProducer { sink, disposable in
+            let task = self.base.downloadImage(with: url, options: options, progress: nil) { image, _, error, _ in
+                guard let image = image else {
+                    let e = error ?? NSError(domain: "", code: 0, userInfo: nil)
+                    sink.send(error: e as NSError)
+                    return
+                }
+                sink.send(value: image)
+                sink.sendCompleted()
+            }
+            disposable.add({
+                task?.cancel()
+            })
+        }
+    }
+}
+
 private struct AssosiationKey {
     static var imageUrl: NSString = "sd.imageUrl"
 }
 
 extension UIImageView {
+    @available(*, deprecated, renamed: "reactive.imageURL")
     public var rac_imageUrl: MutableProperty<URL?> {
         return lazyMutableProperty(self, &AssosiationKey.imageUrl, { [unowned self] url in
             self.sd_setImage(with: url, placeholderImage: nil)
@@ -15,6 +42,7 @@ extension UIImageView {
 }
 
 extension SDWebImageDownloader {
+    @available(*, deprecated, renamed: "reactive.downloadImage(with:options:)")
     public func rac_downloadImageWithURL(url: URL, options: SDWebImageDownloaderOptions = SDWebImageDownloaderOptions()) -> SignalProducer<UIImage, NSError> {
         return SignalProducer { sink, disposable in
             let task = self.downloadImage(with: url, options: options, progress: nil) { image, _, error, _ in
