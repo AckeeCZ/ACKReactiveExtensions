b8dd009fc476e09f14eef98fa44d1ba689db735a
Jakub Olejník <olejnjak@gmail.com>
2017-03-09 16:28:44 +0100

    Docs for WebKit

--- ACKReactiveExtensions/WebKit/WebKitExtensions.swift
+++ ACKReactiveExtensions/WebKit/WebKitExtensions.swift
@@ -4,6 +4,7 @@ import ReactiveCocoa
 import ACKReactiveExtensions
 
 extension Reactive where Base: WKWebView {
+    /// Property which observes estimated progress of request load
     public var estimatedProgress: Property<CGFloat> {
         return Property(base.rac_estimatedProgress.map { $0 ?? 0 })
     }
@@ -15,9 +16,7 @@ private enum WebKitKeys {
 
 extension WKWebView {
 
-    /**
-     * Property which observes estimated progress of request load.
-     */
+    /// Property which observes estimated progress of request load
     @available(*, deprecated, renamed: "reactive.estimatedProgress")
     public var rac_estimatedProgress: DynamicProperty<CGFloat> {
         return lazyAssociatedProperty(self, &WebKitKeys.estimatedProgress, factory: { [unowned self] _ in
