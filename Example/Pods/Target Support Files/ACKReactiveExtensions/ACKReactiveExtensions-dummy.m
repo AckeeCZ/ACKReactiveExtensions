d427c3bbb7502e1f112932686bf679eedcaf48c9
Jakub Olejník <olejnjak@gmail.com>
2017-03-26 01:40:30 +0100

    Remove Pods from .gitignore

---
+++ Example/Pods/Target Support Files/ACKReactiveExtensions/ACKReactiveExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ACKReactiveExtensions : NSObject
+@end
+@implementation PodsDummy_ACKReactiveExtensions
+@end
---
+++ Example/Pods/Target Support Files/Pods-ACKReactiveExtensions_Example-ACKReactiveExtensions_Tests/Pods-ACKReactiveExtensions_Example-ACKReactiveExtensions_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACKReactiveExtensions_Example_ACKReactiveExtensions_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ACKReactiveExtensions_Example_ACKReactiveExtensions_Tests
+@end
