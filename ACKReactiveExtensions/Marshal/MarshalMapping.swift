f2301efd9de6b90ababda1385a107bb626ac31a1
Jakub Olejník <olejnjak@gmail.com>
2017-04-06 16:16:57 +0200

    Add Marshal mapping docs

--- ACKReactiveExtensions/Marshal/MarshalMapping.swift
+++ ACKReactiveExtensions/Marshal/MarshalMapping.swift
@@ -10,11 +10,32 @@ import Result
 import Marshal
 import ReactiveSwift
 
+/**
+ * Protocol that allows creation of custom Marshal errors
+ */
 public protocol MarshalErrorCreatable: Error {
+    
+    /**
+     * Create error containing passed `MarshalError`
+     *
+     * - parameter marshalError: `MarshalError` which should be wrapped
+     */
     static func createMarshalError(_ marshalError: MarshalError) -> Self
 }
 
+extension MarshalError: MarshalErrorCreatable {
+    public static func createMarshalError(_ marshalError: MarshalError) -> MarshalError {
+        return marshalError
+    }
+}
+
 extension SignalProtocol where Value == Any, Error: MarshalErrorCreatable {
+    
+    /**
+     * Map value as `Unmarshaling` object
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType? = nil) -> Signal<Model, Error> where Model: Unmarshaling {
         return attemptMap { json in
             Result {
@@ -33,6 +54,11 @@ extension SignalProtocol where Value == Any, Error: MarshalErrorCreatable {
         }
     }
     
+    /**
+     * Map value as `Unmarshaling` object
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType? = nil) -> Signal<[Model], Error> where Model: Unmarshaling {
         return attemptMap { json in
             Result {
@@ -52,6 +78,11 @@ extension SignalProtocol where Value == Any, Error: MarshalErrorCreatable {
         }
     }
     
+    /**
+     * Map value as `ValueType`
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType) -> Signal<Model, Error> where Model: ValueType {
         return attemptMap { json in
             Result {
@@ -68,14 +99,29 @@ extension SignalProtocol where Value == Any, Error: MarshalErrorCreatable {
 }
 
 extension SignalProducerProtocol where Value == Any, Error: MarshalErrorCreatable {
+    /**
+     * Map value as `Unmarshaling` object
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType? = nil) -> SignalProducer<Model, Error> where Model: Unmarshaling {
         return lift { $0.mapResponseMarshal(for: key) }
     }
     
+    /**
+     * Map value as `Unmarshaling` object
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType? = nil) -> SignalProducer<[Model], Error> where Model: Unmarshaling {
         return lift { $0.mapResponseMarshal(for: key) }
     }
     
+    /**
+     * Map value as `ValueType`
+     *
+     * - parameter key: If your objects are contained within dictionary pass the key here
+     */
     public func mapResponseMarshal<Model>(for key: KeyType) -> SignalProducer<Model, Error> where Model: ValueType {
         return lift { $0.mapResponseMarshal(for: key) }
     }
