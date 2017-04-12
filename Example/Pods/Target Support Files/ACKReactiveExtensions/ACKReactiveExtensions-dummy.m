9f1dbebcc68259eb8de15ffcb8887a68bb324953
Jan Misar <misar.jan@gmail.com>
2017-04-12 09:44:43 +0200

    Ignore Pods folder.

--- Example/Pods/Target Support Files/ACKReactiveExtensions/ACKReactiveExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ACKReactiveExtensions : NSObject
-@end
-@implementation PodsDummy_ACKReactiveExtensions
-@end
--- Example/Pods/Target Support Files/Pods-ACKReactiveExtensions_Example-ACKReactiveExtensions_Tests/Pods-ACKReactiveExtensions_Example-ACKReactiveExtensions_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ACKReactiveExtensions_Example_ACKReactiveExtensions_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ACKReactiveExtensions_Example_ACKReactiveExtensions_Tests
-@end
