d5b99ad409f1221ee17e19a5c325f6ef9138083d
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2017-10-23 17:52:02 +0200

    Remove WebKit deprecations

--- ACKReactiveExtensions/WebKit/WebKitExtensions.swift
+++ ACKReactiveExtensions/WebKit/WebKitExtensions.swift
@@ -12,14 +12,3 @@ extension Reactive where Base: WKWebView {
 private enum WebKitKeys {
     static var estimatedProgress = UInt8(0)
 }
-
-extension WKWebView {
-
-    /// Property which observes estimated progress of request load
-    @available(*, deprecated, renamed: "reactive.estimatedProgress")
-    public var rac_estimatedProgress: DynamicProperty<CGFloat> {
-        return lazyAssociatedProperty(self, &WebKitKeys.estimatedProgress, factory: { [unowned self] _ in
-            return DynamicProperty<CGFloat>(object: self, keyPath: "estimatedProgress")
-        })
-    }
-}
