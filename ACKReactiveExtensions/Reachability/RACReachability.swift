4c53cace698bd8f27540fe1e96ccc4978b55c7a8
Jakub Olejník <jakub.olejnik@ackee.cz>
2017-12-28 16:26:47 +0100

    Update dependencies

--- ACKReactiveExtensions/Reachability/RACReachability.swift
+++ ACKReactiveExtensions/Reachability/RACReachability.swift
@@ -6,39 +6,35 @@
 //  Copyright (c) 2015 Ackee s.r.o. All rights reserved.
 //
 
-import Foundation
-import ReactiveSwift
 import Result
-import ObjectiveC
 import Reachability
+import ReactiveSwift
+
+#if !COCOAPODS
+    import ACKReactiveExtensionsCore
+#endif
 
 extension Reactive where Base: Reachability {
     /// Current network status property
-    public var status: Property<NetworkStatus> {
-        return Property(initial: base.currentReachabilityStatus(), then: statusSignal)
+    public var status: Property<Reachability.Connection> {
+        return Property(initial: base.connection, then: statusSignal)
     }
     
-    private var statusSignal: SignalProducer<NetworkStatus, NoError> {
+    private var statusSignal: SignalProducer<Reachability.Connection, NoError> {
         return lazyAssociatedProperty(base, &AssociationKeys.statusSignalKey) {
-            SignalProducer<NetworkStatus, NoError> { [unowned base = self.base] observer, _ in
-                let oldReachableBlock = base.reachableBlock
-                base.reachableBlock = { reachability in
+            SignalProducer<Reachability.Connection, NoError> { [unowned base = self.base] observer, _ in
+                let oldReachableBlock = base.whenReachable
+                base.whenReachable = { reachability in
                     oldReachableBlock?(reachability)
-                    
-                    if let reachability = reachability {
-                        observer.send(value: reachability.currentReachabilityStatus())
-                    }
+                    observer.send(value: reachability.connection)
                 }
                 
-                let oldUnreachableBlock = base.unreachableBlock
-                base.unreachableBlock = { reachability in
+                let oldUnreachableBlock = base.whenUnreachable
+                base.whenUnreachable = { reachability in
                     oldUnreachableBlock?(reachability)
-                    
-                    if let reachability = reachability {
-                        observer.send(value: reachability.currentReachabilityStatus())
-                    }
+                    observer.send(value: reachability.connection)
                 }
-                }.on(started: { [weak base = self.base] in base?.startNotifier() },
+                }.on(started: { [weak base = self.base] in try? base?.startNotifier() },
                      terminated: { [weak base = self.base] in base?.stopNotifier() })
         }
     }
@@ -47,38 +43,4 @@ extension Reactive where Base: Reachability {
 private struct AssociationKeys {
     static var statusKey: UInt8 = 0
     static var statusSignalKey: UInt8 = 1
-}
-
-extension Reachability {
-    /// Current network status SignalProducer
-    @available(*, deprecated, renamed: "reactive.status.producer")
-    public var rac_status: SignalProducer<NetworkStatus, NoError> {
-        if let signalProducer = (objc_getAssociatedObject(self, &AssociationKeys.statusKey) as? SignalProducer<NetworkStatus, NoError>) {
-            return signalProducer
-        } else {
-            let newProducer = SignalProducer<NetworkStatus, NoError>({ (sink, dis) -> () in
-                sink.send(value: self.currentReachabilityStatus())
-                let oldRBlock = self.reachableBlock
-                self.reachableBlock = { r in
-                    if let oldRBlock = oldRBlock {
-                        oldRBlock(r)
-                    }
-                    if let r = r {
-                        sink.send(value: r.currentReachabilityStatus())
-                    }
-                }
-                let oldUnRBlock = self.unreachableBlock
-                self.unreachableBlock = { r in
-                    if let oldUnRBlock = oldUnRBlock {
-                        oldUnRBlock(r)
-                    }
-                    if let r = r {
-                        sink.send(value: r.currentReachabilityStatus())
-                    }
-                }
-            })
-            objc_setAssociatedObject(self, &AssociationKeys.statusKey, newProducer, .OBJC_ASSOCIATION_RETAIN)
-            return newProducer
-        }
-    }
 }
--- ACKReactiveExtensions/UIKit/TextfieldBinding.swift
+++ ACKReactiveExtensions/UIKit/TextfieldBinding.swift
@@ -6,6 +6,7 @@
 //
 //
 
+import UIKit
 import ReactiveSwift
 
 infix operator <~> : BindingPrecedence
