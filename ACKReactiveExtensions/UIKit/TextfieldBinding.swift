d63a77163c26c8e8d81cb37d56151bc0d681496e
Jakub Olejník <olejnjak@gmail.com>
2018-01-03 21:22:19 +0100

    Fix TextFieldBinding

--- ACKReactiveExtensions/UIKit/TextfieldBinding.swift
+++ ACKReactiveExtensions/UIKit/TextfieldBinding.swift
@@ -13,52 +13,52 @@ infix operator <~> : BindingPrecedence
 
 /// Binds given `property` to continousValues of given `textField`
 public func <~> (property: MutableProperty<String?>, textField: UITextField) {
-    textField.reactive.text <~ property
+    textField.reactive.text <~ property.producer.skipRepeats()
     property <~ textField.reactive.continuousTextValues
 }
 
 /// Binds given `property` to continousValues of given `textField`
 public func <~> (textField: UITextField, property: MutableProperty<String?>) {
-    textField.reactive.text <~ property
+    textField.reactive.text <~ property.producer.skipRepeats()
     property <~ textField.reactive.continuousTextValues
 }
 
 /// Binds given `property` to continousValues of given `textField`
 /// If `textField` sends `nil` value then it is mapped to an empty string
 public func <~> (property: MutableProperty<String>, textField: UITextField) {
-    textField.reactive.text <~ property
+    textField.reactive.text <~ property.producer.skipRepeats()
     property <~ textField.reactive.continuousTextValues.map { $0 ?? "" }
 }
 
 /// Binds given `property` to continousValues of given `textField`
 /// If `textField` sends `nil` value then it is mapped to an empty string
 public func <~> (textField: UITextField, property: MutableProperty<String>) {
-    textField.reactive.text <~ property
+    textField.reactive.text <~ property.producer.skipRepeats()
     property <~ textField.reactive.continuousTextValues.map { $0 ?? "" }
 }
 
 /// Binds given `property` to continousValues of given `textView`
 public func <~> (property: MutableProperty<String?>, textView: UITextView) {
-    textView.reactive.text <~ property
+    textView.reactive.text <~ property.producer.skipRepeats()
     property <~ textView.reactive.continuousTextValues
 }
 
 /// Binds given `property` to continousValues of given `textView`
 public func <~> (textView: UITextView, property: MutableProperty<String?>) {
-    textView.reactive.text <~ property
+    textView.reactive.text <~ property.producer.skipRepeats()
     property <~ textView.reactive.continuousTextValues
 }
 
 /// Binds given `property` to continousValues of given `textView`
 /// If `textView` sends `nil` value then it is mapped to an empty string
 public func <~> (textView: UITextView, property: MutableProperty<String>) {
-    textView.reactive.text <~ property
+    textView.reactive.text <~ property.producer.skipRepeats()
     property <~ textView.reactive.continuousTextValues.map { $0 ?? "" }
 }
 
 /// Binds given `property` to continousValues of given `textView`
 /// If `textView` sends `nil` value then it is mapped to an empty string
 public func <~> (property: MutableProperty<String>, textView: UITextView) {
-    textView.reactive.text <~ property
+    textView.reactive.text <~ property.producer.skipRepeats()
     property <~ textView.reactive.continuousTextValues.map { $0 ?? "" }
 }
