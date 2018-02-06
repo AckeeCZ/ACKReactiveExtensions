c3086322ffc85fd8a0114d325bcb0f2197a3f8b3
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-02-06 11:00:28 +0100

    Cleanup UIKitExtensions structure

---
+++ ACKReactiveExtensions/UIKit/CALayerExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  CALayerExtensions.swift
+//  UIKit
+//
+//  Created by Jakub Olejník on 06/02/2018.
+//
+
+import UIKit
+import ReactiveSwift
+
+extension Reactive where Base: CALayer {
+    /// Binding that represents `borderWidth`
+    public var borderWidth: BindingTarget<CGFloat> {
+        return makeBindingTarget { $0.borderWidth = $1 }
+    }
+    
+    /// Binding that represents `borderColor`
+    public var borderColor: BindingTarget<CGColor> {
+        return makeBindingTarget { $0.borderColor = $1 }
+    }
+}
---
+++ ACKReactiveExtensions/UIKit/UIViewExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtensions.swift
+//  Core
+//
+//  Created by Jakub Olejník on 06/02/2018.
+//
+
+import UIKit
+import Result
+import ReactiveSwift
+
+extension Reactive where Base: UIView {
+    /// Property that represents `isHidden`
+    public var isHiddenProperty: Property<Bool> {
+        return Property(initial: base.isHidden, then: isHiddenSignal)
+    }
+    
+    private var isHiddenSignal: Signal<Bool, NoError> {
+        return signal(forKeyPath: "hidden").map { $0 as? Bool }.skipNil()
+    }
+}
