1b81aca58996fb3b284dea1602bbdb59ed4e4289
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-04-15 15:41:17 +0200

    Add `imageView.reactive.imageURL` for AlamofireImage

---
+++ ACKReactiveExtensions/AlamofireImage/AlamofireImageExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireImageExtensions.swift
+//  AlamofireImage
+//
+//  Created by Jakub Olejník on 15/04/2018.
+//
+
+import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import AlamofireImage
+
+extension Reactive where Base: UIImageView {
+    var imageURL: BindingTarget<URL> {
+        return makeBindingTarget { $0.af_setImage(withURL: $1) }
+    }
+}
