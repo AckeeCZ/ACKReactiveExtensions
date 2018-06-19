d6413316954c2a07d690a6f7ce874ec9d03b02de
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-06-19 17:50:12 +0200

    Fix `Action.run()` extension

--- ACKReactiveExtensions/Core/ActionExtensions.swift
+++ ACKReactiveExtensions/Core/ActionExtensions.swift
@@ -5,11 +5,7 @@ extension Action {
     ///
     /// If action is already executing it just binds to its values and errors.
     public func run(_ input: Input) -> SignalProducer<Output, Error> {
-        let completed = SignalProducer(self.completed).map { Signal<Output, Error>.Event.completed }.take(first: 1)
-        let error = SignalProducer(self.errors).map { Signal<Output, Error>.Event.failed($0) }.take(first: 1)
-        let values = SignalProducer(self.values).map { Signal<Output, Error>.Event.value($0) }
-        return SignalProducer.merge(values, completed, error).dematerialize()
-            .on(started: { [weak self] in self?.apply(input).start() })
+        return events.dematerialize().producer.on(started: { [weak self] in self?.apply(input).start() })
     }
 }
 
