e3cc2a7a6398e5496a2600e8f32880ba01b3a3e6
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-07-02 14:47:29 +0200

    Fix `isEnabled` on `Action.run()`

--- ACKReactiveExtensions/Core/ActionExtensions.swift
+++ ACKReactiveExtensions/Core/ActionExtensions.swift
@@ -5,7 +5,15 @@ extension Action {
     ///
     /// If action is already executing it just binds to its values and errors.
     public func run(_ input: Input) -> SignalProducer<Output, Error> {
-        return events.dematerialize().producer.on(started: { [weak self] in self?.apply(input).start() })
+        let events = self.events
+        return Property.combineLatest(isExecuting, isEnabled).producer.take(first: 1)
+            .flatMap(.latest) { isExecuting, isEnabled -> SignalProducer<Output, Error> in
+                switch (isExecuting, isEnabled) {
+                case (true, _): return events.dematerialize().producer
+                case (false, false): return SignalProducer<Output, Error>.interrupted()
+                case (false, true): return events.dematerialize().producer.on(started: { [weak self] in self?.apply(input).start() })
+                }
+        }
     }
 }
 
