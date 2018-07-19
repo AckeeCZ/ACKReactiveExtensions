7df959876572032f8c01419c389a61b067778a0c
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-07-19 14:38:01 +0200

    Try to fix `action.run()` again

--- ACKReactiveExtensions/Core/ActionExtensions.swift
+++ ACKReactiveExtensions/Core/ActionExtensions.swift
@@ -6,12 +6,17 @@ extension Action {
     /// If action is already executing it just binds to its values and errors.
     public func run(_ input: Input) -> SignalProducer<Output, Error> {
         let events = self.events
+        var disposable: Disposable? = nil
         return Property.combineLatest(isExecuting, isEnabled).producer.take(first: 1)
             .flatMap(.latest) { isExecuting, isEnabled -> SignalProducer<Output, Error> in
                 switch (isExecuting, isEnabled) {
                 case (true, _): return events.dematerialize().producer
                 case (false, false): return SignalProducer<Output, Error>.interrupted()
-                case (false, true): return events.dematerialize().producer.on(started: { [weak self] in self?.apply(input).start() })
+                case (false, true): return events.dematerialize().producer
+                    .on(
+                        started: { [weak self] in disposable = self?.apply(input).start() },
+                        disposed: { disposable?.dispose() }
+                    )
                 }
         }
     }
