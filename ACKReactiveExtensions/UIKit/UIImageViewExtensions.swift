199df0bc98f320631745555606baf76e3c882b95
Jakub Olejník <jakub.olejnik@ackee.cz>
2018-09-06 18:03:25 +0200

    Add `imageView.reactive.imageSignal`

---
+++ ACKReactiveExtensions/UIKit/UIImageViewExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIImageViewExtensions.swift
+//  UIKit
+//
+//  Created by Jakub Olejník on 06/09/2018.
+//
+
+import UIKit
+import ReactiveSwift
+import enum Result.NoError
+
+extension Reactive where Base: UIImageView {
+    var imageSignal: Signal<UIImage?, NoError> {
+        return signal(forKeyPath: "image").map { $0 as? UIImage }
+    }
+}
