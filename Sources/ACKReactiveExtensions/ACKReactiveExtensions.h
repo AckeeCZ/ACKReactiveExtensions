1b37e324a36dad0062aad19d0cb52a978cac3a54
Jakub Olejník <jakub.olejnik@ackee.cz>
2024-03-13 14:07:36 +0100

    🔧 Use single target (#63)
    * 🔧 Use single target
    
    * 🔧 Use macOS 14 image
    
    * 🔧 Use new Xcode project file

---
+++ Sources/ACKReactiveExtensions/ACKReactiveExtensions.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACKategories.
+FOUNDATION_EXPORT double ACKReactiveExtensionsVersionNumber;
+
+//! Project version string for ACKategories.
+FOUNDATION_EXPORT const unsigned char ACKReactiveExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACKategories/PublicHeader.h>
+
